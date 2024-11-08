168e6a4ad07a515f83fb8648bcc38d4677f224af
Hugo Rangel <hr2780920@gmail.com>
2024-11-08 07:16:55 -0600

    ee

--- Parcial 3/HolaMundo/Hola Mundo.js
+++ Parcial 3/HolaMundo/Hola Mundo.js
@@ -3,7 +3,7 @@ const app =express();
 
 
 app.get('/', (req, res) => {
-    res.send('Hola Mundo')
+    res.send('Hola Mundo :o')
    })
    app.listen(3000, () => {
     console.log(`Hola Mundo en el puerto: ${3000}`)

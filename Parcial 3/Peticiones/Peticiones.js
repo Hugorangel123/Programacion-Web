efa7d11e7c6298de754604bf5291a662970717d0
Hugo Rangel <hr2780920@gmail.com>
2024-11-11 07:47:01 -0600

    Pug

---
+++ Parcial 3/Peticiones/Peticiones.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app =express();
+
+
+
+app.use(express.json())
+app.get('/', (req, res) => {
+   
+    res.send('Hola Mundo :o')
+   })
+   app.listen(3000, () => {
+    console.log(`Hola Mundo en el puerto: ${3000}`)
+   })
+
+
+app.get('/Estudiantes/', (req, res) => {
+   console.info(req.query)
+    res.send(`Estudiante ${req.query.control}`)
+})
+
+app.get('/Maestros', (req, res) => {
+    console.info(req.body)
+     res.send(`Maestros`)
+ })
+
+app.get('/Maestros/:id', (req, res) => {
+    console.info(req.params.id)
+     res.send(`Maestro con id ${req.params.id}`)
+ })
+
+   
